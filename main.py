238d65f06e8ac8d1efd7fc31d146536936a6b067
mamama <39580104+mamama039356@users.noreply.github.com>
2020-04-04 23:34:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import re
 import pandas as pd
+import itertools
+import numpy as np
+import argparseInput as arg
+import os
 
-f = open("./untitled.txt", encoding="utf-8")
+path_infile = arg.commandline()
+f = open(path_infile, mode="r", encoding="utf-8")
 line = f.readline()
+count = 1
+filename, ext = os.path.splitext(path_infile)
+path_outfile = filename+"s"+ext
+g = open(path_outfile, mode="w", encoding="utf-8")
+
 
 regex_response = r"応答：.+"
 pattern_response = re.compile(regex_response)
@@ -12,51 +22,69 @@ regex_level = r"水準：.+"
 pattern_level = re.compile(regex_level)
 regex_repeat = r"繰り返し数：.+"
 pattern_repeat = re.compile(regex_repeat)
+regex_table = "\|"
+pattern_table = re.compile(regex_table)
 
 while line:
-#     print(line)
-    factor_dict = {}
     m1 = pattern_response.match(line)
     if m1:
         response = m1.group()[3:]
-        print(response)
+        # file ope.
+        g.write(line)
         line = f.readline()
+        count+=1
         m2 = pattern_factor.match(line)
         if m2:
             factor = m2.group()[3:]
             factor = factor.split("/")
-            print(factor)
             factor_num = len(factor)
+            # file ope.
+            g.write(line)
             line = f.readline()
+            count+=1
             m3 = pattern_level.match(line)
             if m3:
                 level = m3.group()[3:]
                 level = level.split("/")
-                print(level)
-                level_nums = []
-                level_crossover = 1
+                levels_list = []
                 for i in range(factor_num):
-                    levels = level[i].split(",") # ここに繰り返しをかける
-                    factor_dict[factor[i]] = pd.Series(levels)
-                    level_nums.append(len(levels))
-                    level_crossover *= level_nums[i]
-                    print(level_crossover)
-                for j, k in enumerate(level_nums):
-                    level_nums[j] = level_crossover/k
-                print(factor_dict)
-                print(level_nums)
-                # markdown仕様
-                # 繰り返し処理
-                df = pd.DataFrame(factor_dict)
-                print(df)
-                
+                    levels = level[i].split(",")
+                    levels_list.append(levels)
+                plan = list(itertools.product(*levels_list))
+                no = np.arange(start=1, stop=len(plan)+1).tolist()
+                df = pd.DataFrame(plan, index=no, columns=factor)
+                print(df) # plan
+                # file ope.
+                g.write(line)
                 line = f.readline()
+                count+=1
                 m4 = pattern_repeat.match(line)
                 if m4:
                     repeat = m4.group()[6:]
-                    print(repeat)    
-    line = f.readline()
-f.close()        
-#     factor = 
-#     level = 
-#     repeat =
+                    # 上書き設定を追加
+                    g.write(line)
+                    line = f.readline()
+                    count+=1
+                    g.write(line)
+                    line = f.readline()
+                    count+=1
+                    m5 = pattern_table.match(line)
+                    if m5:
+                        g.write(line)
+                        line = f.readline()
+                        count += 1
+                    else:
+                        # 書き込みを入れる
+                        g.write(df.to_markdown())
+                        g.write("\n"*2)
+                        # 読み込みを書き込む
+                        g.write(line)
+                        line=f.readline()
+                        count += 1
+    else:
+        g.write(line)
+        line = f.readline()
+        count+=1
+f.close()
+g.close()
+
